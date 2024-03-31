684be79e4f5708618f82a51411eec12f35aa7b87
Kevin Enrique Coloma Fonseca <kevin.coloma@epn.edu.ec>
2024-03-31 12:29:14 -0500

    Fixed Problem Cors Imported

--- models/movie.js
+++ models/movie.js
@@ -2,7 +2,7 @@ import { createRequire } from 'node:module'
 import { randomUUID } from 'node:crypto'
 
 const require = createRequire(import.meta.url)
-const movies = require('./movies.json')
+const movies = require('../movies.json')
 
 export class MovieModel {
   static async getAll ({ genre }) {
